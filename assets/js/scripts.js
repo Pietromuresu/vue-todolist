0241830fa91200cec21a4228f38f2223fe84adba
Pietromuresu <muresupietro66@gmail.com>
2023-04-14 16:46:35 +0200

    Delete tasks if done + error message if isn't done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,17 +11,35 @@ createApp ({
         },
         {
           text: "rinnovare l'assicurazione alla macchina",
-          done: true
-        },
-        {
-          text: 'fare i compiti',
           done: false
         },
         {
           text: 'fare i compiti',
           done: false
         },
-      ]
+        {
+          text: 'fare i compiti',
+          done: false
+        },
+      ],
+      message: ''
     } 
+  },
+  methods: {
+    removeTask(task, index){
+      if(task.done){
+        this.tasks.splice(index, 1)
+      }else {
+        this.errorMessage('Non hai completato il task')
+      }
+      
+    },
+
+    errorMessage(msg){
+      this.message = msg
+      setTimeout(() => {
+       this.message= ''
+      }, 2000)
+    }
   }
 }).mount('#app')
