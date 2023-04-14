863e02ca2859c152b7db56c472d968be9626e5cd
Pietromuresu <muresupietro66@gmail.com>
2023-04-14 16:15:26 +0200

    Task added with vue + toggle class done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,7 +4,24 @@ const {createApp} = Vue;
 createApp ({
   data(){
     return{
-
+      tasks: [
+        {
+          text: "fare i compiti",
+          done: false
+        },
+        {
+          text: "rinnovare l'assicurazione alla macchina",
+          done: true
+        },
+        {
+          text: 'fare i compiti',
+          done: false
+        },
+        {
+          text: 'fare i compiti',
+          done: false
+        },
+      ]
     } 
   }
 }).mount('#app')
